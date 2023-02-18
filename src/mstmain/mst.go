930a81986196499910707700507946c27f4cdcd9
tom <thomas.teplick@hotmail.com>
2023-02-18 09:34:07 -0700

    Finished implementation. 2-500 vertices allowed, Distance and starting vertex coordinates shown in Euclidean Graph Options.

--- src/mstmain/mst.go
+++ src/mstmain/mst.go
@@ -23,18 +23,21 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 const (
-	addr           = "127.0.0.1:8080"         // http server listen address
-	fileConnect    = "templates/primmst.html" // html for Prim MST
-	patternConnect = "/primmst"               // http handler for Prim MST
-	rows           = 300                      // #rows in grid
-	columns        = 300                      // #columns in grid
-	xlabels        = 11                       // # labels on x axis
-	ylabels        = 11                       // # labels on y axis
-	dataDir        = "data/"                  // directory for the data files
-	fileVerts      = "vertices.csv"           // bounds and complex locations of vertices
+	addr                = "127.0.0.1:8080"              // http server listen address
+	filePrimMST         = "templates/primmst.html"      // html for Prim MST
+	fileGraphOptions    = "templates/graphoptions.html" // html for Graph Options
+	patternPrimMST      = "/primmst"                    // http handler for Prim MST
+	patternGraphOptions = "/graphoptions"               // http handler for Graph Options
+	rows                = 300                           // #rows in grid
+	columns             = rows                          // #columns in grid
+	xlabels             = 11                            // # labels on x axis
+	ylabels             = 11                            // # labels on y axis
+	dataDir             = "data/"                       // directory for the data files
+	fileVerts           = "vertices.csv"                // bounds and complex locations of vertices
 )
 
 // Edges are the vertices of the edge endpoints
@@ -59,13 +62,20 @@ type MST []*Edge
 
 // Type to contain all the HTML template actions
 type PlotT struct {
-	Grid   []string // plotting grid
-	Status string   // status of the plot
-	Xlabel []string // x-axis labels
-	Ylabel []string // y-axis labels
+	Grid          []string // plotting grid
+	Status        string   // status of the plot
+	Xlabel        []string // x-axis labels
+	Ylabel        []string // y-axis labels
+	Distance      string   // MST total distance
+	Vertices      string   // number of vertices
+	Xmin          string   // x minimum endpoint in Euclidean graph
+	Xmax          string   // x maximum endpoint in Euclidean graph
+	Ymin          string   // y minimum endpoint in Euclidean graph
+	Ymax          string   // y maximum endpoint in Euclidean graph
+	StartLocation string   // start vertex location in x,y coordinates
 }
 
-// Type to hold the minimum and maximum data values
+// Type to hold the minimum and maximum data values of the Euclidean graph
 type Endpoints struct {
 	xmin float64
 	xmax float64
@@ -75,10 +85,10 @@ type Endpoints struct {
 
 // PrimMST type used by the http handler methods to create the MST
 type PrimMST struct {
-	graph    [][]float64  // matrix of vertices and their distance from each other
-	location []complex128 // complex point(x,y) coordinates
-	mst      MST
-	Endpoints
+	graph     [][]float64  // matrix of vertices and their distance from each other
+	location  []complex128 // complex point(x,y) coordinates of vertices
+	mst       MST
+	Endpoints // Euclidean graph endpoints
 }
 
 // global variables for parse and execution of the html template and MST construction
@@ -89,7 +99,7 @@ var (
 
 // init parses the html template fileS
 func init() {
-	tmplForm = template.Must(template.ParseFiles(fileConnect))
+	tmplForm = template.Must(template.ParseFiles(filePrimMST))
 }
 
 // generateVertices creates random vertices in the complex plane
@@ -118,12 +128,12 @@ func (p *PrimMST) generateVertices(r *http.Request) error {
 			fmt.Printf("String %s conversion to float error: %v\n", values[1], err)
 			return err
 		}
-		if xmax, err = strconv.ParseFloat(values[0], 64); err != nil {
+		if xmax, err = strconv.ParseFloat(values[2], 64); err != nil {
 			fmt.Printf("String %s conversion to float error: %v\n", values[2], err)
 			return err
 		}
 
-		if ymax, err = strconv.ParseFloat(values[1], 64); err != nil {
+		if ymax, err = strconv.ParseFloat(values[3], 64); err != nil {
 			fmt.Printf("String %s conversion to float error: %v\n", values[3], err)
 			return err
 		}
@@ -155,31 +165,39 @@ func (p *PrimMST) generateVertices(r *http.Request) error {
 	// or read in from a previous graph when using a new start vertex.
 	// Insert vertex complex coordinates into locations
 	str := r.FormValue("xmin")
-	xmin, err := strconv.ParseFloat(r.FormValue("xmin"), 64)
+	xmin, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		fmt.Printf("String %s conversion to float error: %v\n", str, err)
 		return err
 	}
 
 	str = r.FormValue("ymin")
-	ymin, err := strconv.ParseFloat(r.FormValue("ymin"), 64)
+	ymin, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		fmt.Printf("String %s conversion to float error: %v\n", str, err)
 		return err
 	}
 
 	str = r.FormValue("xmax")
-	xmax, err := strconv.ParseFloat(r.FormValue("xmax"), 64)
+	xmax, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		fmt.Printf("String %s conversion to float error: %v\n", str, err)
 		return err
 	}
 
 	str = r.FormValue("ymax")
-	ymax, err := strconv.ParseFloat(r.FormValue("ymax"), 64)
+	ymax, err := strconv.ParseFloat(str, 64)
 	if err != nil {
 		fmt.Printf("String %s conversion to float error: %v\n", str, err)
 		return err
+	}
+
+	// Check if xmin < xmax and ymin < ymax and correct if necessary
+	if xmin >= xmax {
+		xmin, xmax = xmax, xmin
+	}
+	if ymin >= ymax {
+		ymin, ymax = ymax, ymin
 	}
 
 	p.Endpoints = Endpoints{xmin: xmin, ymin: ymin, xmax: xmax, ymax: ymax}
@@ -243,18 +261,18 @@ func (p *PrimMST) findDistances() error {
 }
 
 // A PriorityQueue implements heap.Interface and holds Items
-func (pq *PriorityQueue) Len() int {
-	return len(*pq)
+func (pq PriorityQueue) Len() int {
+	return len(pq)
 }
 
-func (pq *PriorityQueue) Less(i, j int) bool {
-	return (*pq)[i].distance < (*pq)[j].distance
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].distance < pq[j].distance
 }
 
-func (pq *PriorityQueue) Swap(i, j int) {
-	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
-	(*pq)[i].index = i
-	(*pq)[j].index = j
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], (pq)[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
 }
 
 // Push inserts an Item in the queue
@@ -282,10 +300,10 @@ func (pq *PriorityQueue) update(item *Item, distance float64) {
 	heap.Fix(pq, item.index)
 }
 
-// findMST finds the minimum spanning tree using Prim's algorithm
-func (p PrimMST) findMST() error {
+// findMST finds the minimum spanning tree (MST) using Prim's algorithm
+func (p *PrimMST) findMST() error {
 	vertices := len(p.location)
-	edgeTo := make([]Edge, vertices)
+	p.mst = make(MST, vertices)
 	marked := make([]bool, vertices)
 	distTo := make([]float64, vertices)
 	for i := range distTo {
@@ -305,7 +323,7 @@ func (p PrimMST) findMST() error {
 			}
 			if dist < distTo[w] {
 				// Edge to w is new best connection from MST to w
-				edgeTo[w] = Edge{v: v, w: w}
+				p.mst[w] = &Edge{v: v, w: w}
 				distTo[w] = dist
 				// Check if already in the queue and update
 				item, ok := pq[w]
@@ -320,30 +338,31 @@ func (p PrimMST) findMST() error {
 		}
 	}
 
-	// Starting index is 0, distance is 0, put it in the queue
-	distTo[0] = 0.0
-	pq[0] = &Item{index: 0, distance: 0.0, Edge: Edge{v: 0, w: 0}}
+	// Starting index is 0, distance is MaxFloat64, put it in the queue
+	distTo[0] = math.MaxFloat64
+	pq[0] = &Item{index: 0, distance: math.MaxFloat64, Edge: Edge{v: 0, w: 0}}
 	heap.Init(&pq)
 
-	// Loop until the queue is empty
+	// Loop until the queue is empty and the MST is finished
 	for len(pq) > 0 {
 		item := heap.Pop(&pq).(*Item)
-		visit(item.v)
+		visit(item.w)
 	}
 
 	return nil
 }
 
-// plotMST draws the MST onto the canvas (grid)
+// plotMST draws the MST onto the grid
 func (p *PrimMST) plotMST(w http.ResponseWriter, status []string) error {
 
 	// Apply the parsed HTML template to plot object
 	// Construct x-axis labels, y-axis labels, status message
 
 	var (
-		plot   PlotT
-		xscale float64
-		yscale float64
+		plot     PlotT
+		xscale   float64
+		yscale   float64
+		distance float64
 	)
 	plot.Grid = make([]string, rows*columns)
 	plot.Xlabel = make([]string, xlabels)
@@ -353,26 +372,78 @@ func (p *PrimMST) plotMST(w http.ResponseWriter, status []string) error {
 	xscale = (columns - 1) / (p.xmax - p.xmin)
 	yscale = (rows - 1) / (p.ymax - p.ymin)
 
-	// Insert the mst vertices and edges in the canvas
+	// Insert the mst vertices and edges in the grid
 	// loop over the MST vertices
-	for _, e := range p.mst {
-		x := real(p.location[e.v])
-		y := imag(p.location[e.w])
-		row := int((p.ymax-y)*yscale + .5)
-		col := int((x-p.xmin)*xscale + .5)
-		plot.Grid[row*columns+col] = "vertex"
-	}
 
 	// color the vertices black
 	// color the edges connecting the vertices gray
+	// color the MST start vertex green
 	// create the line y = mx + b for each edge
-	// translate complex coordinates to row/col on the canvas
-	// translate row/col to slice data object []string
-	// CSS selectors for background-color are "vertex" and "edge"
+	// translate complex coordinates to row/col on the grid
+	// translate row/col to slice data object []string Grid
+	// CSS selectors for background-color are "vertex", "startvertex", and "edge"
+
+	beginEP := complex(p.xmin, p.ymin)  // beginning of the Euclidean graph
+	endEP := complex(p.xmax, p.ymax)    // end of the Euclidean graph
+	lenEP := cmplx.Abs(endEP - beginEP) // length of the Euclidean graph
+
+	for _, e := range p.mst[1:] {
+
+		// Insert the edge between the vertices v, w.  Do this before marking the vertices.
+		// CSS colors the edge gray.
+		beginEdge := p.location[e.v]
+		endEdge := p.location[e.w]
+		lenEdge := cmplx.Abs(endEdge - beginEdge)
+		distance += lenEdge
+		ncells := int(columns * lenEdge / lenEP) // number of points to plot in the edge
+
+		beginX := real(beginEdge)
+		endX := real(endEdge)
+		deltaX := endX - beginX
+		stepX := deltaX / float64(ncells)
+
+		beginY := imag(beginEdge)
+		endY := imag(endEdge)
+		deltaY := endY - beginY
+		stepY := deltaY / float64(ncells)
+
+		// loop to draw the edge
+		x := beginX
+		y := beginY
+		for i := 0; i < ncells; i++ {
+			row := int((p.ymax-y)*yscale + .5)
+			col := int((x-p.xmin)*xscale + .5)
+			plot.Grid[row*columns+col] = "edge"
+			x += stepX
+			y += stepY
+		}
+
+		// Mark the edge start vertex v.  CSS colors the vertex black.
+		row := int((p.ymax-beginY)*yscale + .5)
+		col := int((beginX-p.xmin)*xscale + .5)
+		plot.Grid[row*columns+col] = "vertex"
+
+		// Mark the edge end vertex w.  CSS colors the vertex black.
+		row = int((p.ymax-endY)*yscale + .5)
+		col = int((endX-p.xmin)*xscale + .5)
+		plot.Grid[row*columns+col] = "vertex"
+	}
+
+	// Mark the MST start vertex.  CSS colors the vertex green.
+	x := real(p.location[0])
+	y := imag(p.location[0])
+	plot.StartLocation = fmt.Sprintf("(%.2f, %.2f)", x, y)
+	row := int((p.ymax-y)*yscale + .5)
+	col := int((x-p.xmin)*xscale + .5)
+	plot.Grid[row*columns+col] = "startvertex"
+	plot.Grid[(row+1)*columns+col] = "startvertex"
+	plot.Grid[(row-1)*columns+col] = "startvertex"
+	plot.Grid[row*columns+col+1] = "startvertex"
+	plot.Grid[row*columns+col-1] = "startvertex"
 
 	// Construct x-axis labels
 	incr := (p.xmax - p.xmin) / (xlabels - 1)
-	x := p.xmin
+	x = p.xmin
 	// First label is empty for alignment purposes
 	for i := range plot.Xlabel {
 		plot.Xlabel[i] = fmt.Sprintf("%.2f", x)
@@ -381,7 +452,7 @@ func (p *PrimMST) plotMST(w http.ResponseWriter, status []string) error {
 
 	// Construct the y-axis labels
 	incr = (p.ymax - p.ymin) / (ylabels - 1)
-	y := p.ymin
+	y = p.ymin
 	for i := range plot.Ylabel {
 		plot.Ylabel[i] = fmt.Sprintf("%.2f", y)
 		y += incr
@@ -390,7 +461,19 @@ func (p *PrimMST) plotMST(w http.ResponseWriter, status []string) error {
 	// Status
 	if len(status) > 0 {
 		plot.Status = strings.Join(status, ", ")
+	} else {
+		plot.Status = "Check new start vertex for another MST using the same vertices"
 	}
+
+	// Distance of the MST
+	plot.Distance = fmt.Sprintf("%.2f", distance)
+
+	// Endpoints and Vertices
+	plot.Vertices = strconv.Itoa(len(p.location))
+	plot.Xmin = fmt.Sprintf("%.2f", p.xmin)
+	plot.Xmax = fmt.Sprintf("%.2f", p.xmax)
+	plot.Ymin = fmt.Sprintf("%.2f", p.ymin)
+	plot.Ymax = fmt.Sprintf("%.2f", p.ymax)
 
 	// Write to HTTP using template and grid
 	if err := tmplForm.Execute(w, plot); err != nil {
@@ -398,6 +481,11 @@ func (p *PrimMST) plotMST(w http.ResponseWriter, status []string) error {
 	}
 
 	return nil
+}
+
+// HTTP handler for /graphoptions connections
+func handleGraphOptions(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/graphoptions.html")
 }
 
 // HTTP handler for /primmst connections
@@ -414,7 +502,7 @@ func handlePrimMST(w http.ResponseWriter, r *http.Request) {
 	// Insert vertex complex coordinates into locations
 	err := primmst.generateVertices(r)
 	if err != nil {
-		fmt.Printf("generateVertices error: %v", err)
+		fmt.Printf("generateVertices error: %v\n", err)
 		status = append(status, err.Error())
 	}
 
@@ -432,7 +520,7 @@ func handlePrimMST(w http.ResponseWriter, r *http.Request) {
 		status = append(status, err.Error())
 	}
 
-	// Draw MST into 300 x 300 cell 2px canvas
+	// Draw MST into 300 x 300 cell 2px grid
 	// Construct x-axis labels, y-axis labels, status message
 	err = primmst.plotMST(w, status)
 	if err != nil {
@@ -443,8 +531,10 @@ func handlePrimMST(w http.ResponseWriter, r *http.Request) {
 
 // main sets up the http handlers, listens, and serves http clients
 func main() {
-	// Set up http server with handler for Prim MST execution
-	http.HandleFunc(patternConnect, handlePrimMST)
+	rand.Seed(time.Now().Unix())
+	// Set up http servers with handler for Graph Options and Prim MST
+	http.HandleFunc(patternPrimMST, handlePrimMST)
+	http.HandleFunc(patternGraphOptions, handleGraphOptions)
 	fmt.Printf("Prim MST Server listening on %v.\n", addr)
 	http.ListenAndServe(addr, nil)
 }
